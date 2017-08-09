40b93a06f4912b0398ad3107c9e6d2963f25a8c0
James Gardiner <james@jgardiner.co.uk>
2017-08-09 15:11:47 +0100

    More logging

--- app/response_processor.py
+++ app/response_processor.py
@@ -86,9 +86,16 @@ class ResponseProcessor:
         elif res.status_code == 400:
             self.logger.info("Returned from service",
                              response="client error")
+            self.logger.debug("Response code: {}".format(res.status_code),
+                              "Response content: {}".format(res.content))
             raise BadMessageError
 
         else:
             self.logger.error("Returned from service",
                               response="service error")
-            raise RetryableError
+            self.logger.debug("Response code: {}".format(res.status_code),
+                              "Response content: {}".format(res.content))
+            """This needs to be a RetryableError in any kind of preprod
+            or prod environment. Only setting to BadMessage for debugging,
+            temporarily"""
+            raise BadMessageError
